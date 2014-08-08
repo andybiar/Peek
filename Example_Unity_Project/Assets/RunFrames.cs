483c1911c6c97c82a40cadf2fdb3f144a047a963
Andy Biar <andybiar@Andys-MacBook-Pro.local>
2014-08-07 19:44:55 -0700

    fixed the build while bored on a plane :D

---
+++ Example_Unity_Project/Assets/RunFrames.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class RunFrames : MonoBehaviour {
+		
+	ArrayList imageBuffer = new ArrayList();
+	int frameNum;
+	
+	//Loads all frames into buffer from directory
+	private void LoadImages()
+	{
+	    string pathPrefix = @"file://";
+	    string pathImageAssets = Application.dataPath + "/depthPNGs";
+	    string filename = @"save.";
+	    string fileSuffix = @".png";
+	     
+	    for (int i=0; i < 97; i++)
+	    {
+	    	string indexSuffix = "";
+	    	float logIdx = Mathf.Log10(i+1);
+	    	
+	    	if (logIdx < 1.0)
+	    		indexSuffix += "0000";
+	    	else if (logIdx < 2.0)
+	    		indexSuffix += "000";
+			else if (logIdx < 3.0)
+	    		indexSuffix += "00";
+			else if (logIdx < 4.0)
+	    		indexSuffix += "0";
+	    	
+	    	
+	    	indexSuffix += (i+1);
+	     
+	    	string fullFilename = pathPrefix + pathImageAssets + filename + indexSuffix + fileSuffix;
+	     
+	    	//WWW www = new WWW(fullFilename);
+	    	//Texture2D texTmp = new Texture2D(512, 1024, TextureFormat.DXT5, false);
+	    	//LoadImageIntoTexture compresses JPGs by DXT1 and PNGs by DXT5
+	    	//www.LoadImageIntoTexture(texTmp);
+
+			Texture2D resLoad = Resources.Load<Texture2D>("andy_demo/save." + indexSuffix);
+	     
+	    	imageBuffer.Add(resLoad);
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 24;
+		LoadImages();
+		frameNum = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Texture2D temp = (Texture2D)imageBuffer[frameNum];
+		renderer.material.SetTexture("_MainTex", temp);
+		frameNum = (frameNum + 1)%97;
+	}
+}
